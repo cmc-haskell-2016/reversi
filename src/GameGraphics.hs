c66358bb0765b2d2c2192525111fa25f550a6bbe
Assylzhan Shabkhatov <strelokask@mail.ru>
2016-05-05 13:51:40 +0300

    added src

--- src/GameGraphics.hs
+++ src/GameGraphics.hs
@@ -1,4 +1,27 @@
 module GameGraphics where
+
 import Types
+import GameLogic
+
+import Graphics.Gloss
+
 gameStart :: IO()
-gameStart = undefined
+gameStart = display --play display bgColor numSimStep initWorld worldToPicture indivInputEvents
+
+display :: Display 
+display = InWindow "Reversi" (600, 800) (0, 0) 
+
+bgColor :: Color 
+bgColor = green
+
+numSimStep :: Int 
+numSimStep = 10
+
+initWorld :: createWorld
+initWorld = undefined
+
+worldToPicture :: Object -> Picture
+worldToPicture = undefined
+
+indivInputEvents :: Event -> World -> World
+indivInputEvents = undefined
--- src/Types.hs
+++ src/Types.hs
@@ -1,3 +1,5 @@
 module Types where
 type Figure = (Int, Int)
+type FilePath =  String
 type World = (Figure, Bool)
+type Object = (Int, Int)
