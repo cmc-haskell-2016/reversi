454b0d28afab359cd781b464f6cf199030fc6cdf
Assylzhan Shabkhatov <strelokask@mail.ru>
2016-05-05 11:38:41 +0300

    added APP

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,7 @@
-module Main(main) where
+module Main where
+import 	Types
+import GameGraphics
+import GameLogic
 
-import Graphics.Gloss
-
-window :: Display
-window = InWindow "Nice Window" (200, 200) (10, 10)
-
-background :: Color
-background = white
-
-drawing :: Picture
-drawing = circle 80
-
-main :: IO ()
-main = display window background drawing
+main :: IO()
+main = gameStart
